1c921b44bb62769408d17b50292a19e7c51f2c97
luke <kyloluke88@gmail.com>
2022-08-22 03:33:11 +0800

    add config init

---
+++ bootstrap/route.go
@@ -0,0 +1,38 @@
+package bootstrap
+
+import (
+	"gohub/routes"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(router *gin.Engine) {
+	// 注册全局中简介
+	registerGlobalMiddleWare(router)
+
+	// 注册 API 路由
+	routes.RegisterAPIRoutes(router)
+
+	//  配置 404 路由
+	setup404Handler(router)
+}
+
+func registerGlobalMiddleWare(router *gin.Engine) {
+	router.Use(gin.Logger(), gin.Recovery())
+}
+
+func setup404Handler(router *gin.Engine) {
+	router.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "返回 404 页面")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确",
+			})
+		}
+	})
+}
---
+++ routes/api.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"gohub/pkg/config"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// RegisterAPIRoutes 注册网页相关路由
+func RegisterAPIRoutes(r *gin.Engine) {
+	// 注册一个路由
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gin.Context) {
+			// 以 JSON 格式响应
+
+			app_name := config.GetString("app.name")
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "app name : " + app_name,
+			})
+		})
+	}
+
+}
