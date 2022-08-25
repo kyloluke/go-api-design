2c3bee1b580dd475141208209aa4e468e610a4e5
luke <kyloluke88@gmail.com>
2022-08-26 03:56:47 +0800

    req_data_bind and validate restructure

--- app/http/controllers/v1/auth/signup_controller.go
+++ app/http/controllers/v1/auth/signup_controller.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"fmt"
 	v1 "gohub/app/http/controllers/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
@@ -16,29 +15,13 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-
 	// 初始化请求对象
 	request := requests.SignupPhoneExistRequest{}
 
-	// 解析 JSON 格式，非json格式抛出错误
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		fmt.Println(err.Error())
+	if ok := requests.Validate(c, &request, requests.ValidatePhoneIsExist); !ok {
 		return
 	}
 
-	// 表单验证
-	errs := requests.ValidatePhoneIsExist(&request)
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
-		return
-	}
 	c.JSON(http.StatusOK, gin.H{
 		"exists": user.IsPhoneExist(request.Phone),
 	})
@@ -48,20 +31,7 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 
 	request := requests.SignupEmailExistRequest{}
 
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": err.Error(),
-		})
-		fmt.Println(err.Error())
-		return
-	}
-
-	// 数据验证
-	errs := requests.ValidateIsEmailExist(&request)
-	if len(errs) > 0 {
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
+	if ok := requests.Validate(c, &request, requests.ValidateIsEmailExist); !ok {
 		return
 	}
 
--- app/requests/requests.go
+++ app/requests/requests.go
@@ -1,6 +1,44 @@
 package requests
 
-import "github.com/thedevsaddam/govalidator"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+	"net/http"
+)
+
+// ValidatorFunc 用于控制器的回调函数用
+//ValidatePhoneIsExist() ValidateEmailIsExist() 等函数名称
+
+type ValidatorFunc func(interface{}) map[string][]string
+
+func Validate(c *gin.Context, data interface{}, handler ValidatorFunc) bool {
+	// 解析json，将数据绑定到 结构体字段中
+	if err := c.ShouldBindJSON(data); err != nil {
+		// todo 这里不是会直接返回么，为什么下面还要使用 return false
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
+			"error":   err.Error(),
+		})
+
+		fmt.Println(err.Error())
+		return false //
+	}
+
+	// 调用表单验证
+	errs := handler(data)
+
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"message": "表单验证不通过，具体请查看errors",
+			"errors":  errs,
+		})
+		return false
+	}
+
+	// 商法有错，请求会被直接返回，入国通过则
+	return true
+}
 
 func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
 
