32d94f785482e303da869b8c9c460eb746c36f59
luke <kyloluke88@gmail.com>
2022-08-30 04:32:55 +0800

    add limit middleware

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/spf13/cast v1.5.0
 	github.com/spf13/viper v1.12.0
 	github.com/thedevsaddam/govalidator v1.9.10
+	github.com/ulule/limiter/v3 v3.10.0
 	go.uber.org/zap v1.22.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gorm.io/driver/mysql v1.3.6
@@ -25,6 +26,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/image v0.0.0-20190802002840-cff245a6509b // indirect
--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gohub/app/http/controllers/v1/auth"
+	"gohub/app/http/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,28 +12,34 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 注册一个路由
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("40-H"))
 	{
 		authGroup := v1.Group("/auth")
+		authGroup.Use(middlewares.LimitIP("50-H")) // todo 为什么内层比外层的还多？？为什么这样设计呢
 		{
 			// 注册
 			suc := new(auth.SignupController)
-			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
-			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
-			authGroup.POST("/signup/using-email", suc.SignupUsingEmail) // 通过邮箱验证码注册
+			authGroup.POST("/signup/phone/exist", middlewares.LimitPerRoute("30-H"), middlewares.GuestJWT(), suc.IsPhoneExist)
+			authGroup.POST("/signup/email/exist", middlewares.LimitPerRoute("30-H"), middlewares.GuestJWT(), suc.IsEmailExist)
+			authGroup.POST("/signup/using-email", middlewares.GuestJWT(), suc.SignupUsingEmail) // 通过邮箱验证码注册
 
 			// 发送验证码 图片验证码，需要加限流
 			vcc := new(auth.VerifyCodeController)
-			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)  // 显示图片验证码
-			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail) // 携带正确的图片验证码和输入的邮箱，给邮箱发送验证码
+			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("30-H"), vcc.ShowCaptcha)  // 显示图片验证码
+			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("20-H"), vcc.SendUsingEmail) // 验证图片验证码，验证成功后，给邮箱发送验证码
 
 			// 登录
 			lgc := new(auth.LoginController)
-			authGroup.POST("/login/using-password", lgc.LoginByPassword)
-			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+			authGroup.POST("/login/using-password", middlewares.GuestJWT(), lgc.LoginByPassword)
+			authGroup.POST("/login/refresh-token", middlewares.AuthJWT(), lgc.RefreshToken)
 
 			// 重置密码
 			prc := new(auth.PasswordResetController)
-			authGroup.POST("/password-reset/using-email", prc.ResetByEmail)
+			authGroup.POST("/password-reset/using-email", middlewares.GuestJWT(), prc.ResetByEmail)
 		}
 
 	}
